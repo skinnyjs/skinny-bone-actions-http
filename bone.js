c94d7ce556745894f2c1a99d2424ffc376fe7fb1
Ivan Shumkov <ivan@shumkov.ru>
2014-03-05 16:55:59 +0400

    add bone

---
+++ bone.js
@@ -0,0 +1,16 @@
+var co = require('co');
+var HttpTransport = require('./httpTransport');
+
+module.exports = function attachHttpTransport(skinny, options) {
+    "use strict";
+
+    skinny.httpTransport = new HttpTransport(skinny, options);
+
+    skinny.on('*initialize', function *initializeHttpTransport() {
+        yield skinny.httpTransport.listen();
+    });
+
+    skinny.on('*shutdown', function *shutdownHttpTransport() {
+        yield skinny.httpTransport.close();
+    });
+};
---
+++ httpTransport.js
@@ -0,0 +1,130 @@
+var http = require('http');
+var url = require('url');
+var util = require('util');
+var createError = require('create-error');
+
+var co = require('co');
+var thunkify = require('thunkify');
+
+function HttpTransport(skinny, options) {
+    "use strict";
+
+    this.skinny = skinny;
+    this.options = options;
+
+    this.server = this._createServer();
+
+    if (options.connectionTimeout) {
+        this.server.timeout = options.connectionTimeout;
+    }
+};
+
+var HttpTransportError = createError('HttpTransportError', { responseCode: 500 });
+var ServiceError = createError(HttpTransportError, 'ServiceError', { responseCode: 500 });
+var WrongSecretError = createError(HttpTransportError, 'WrongSecretError', { responseCode: 401 });
+var BadRequestError = createError(HttpTransportError, 'InvalidRequestError', { responseCode: 400 });
+var ActionNotPresent = createError(BadRequestError, 'ActionNotPresent');
+
+HttpTransport.prototype.ERRORS = {
+    HttpTransportError: HttpTransportError,
+    ServiceError:       ServiceError,
+    WrongSecretError:   WrongSecretError,
+    BadRequestError:    BadRequestError,
+    ActionNotPresent:   ActionNotPresent
+};
+
+HttpTransport.prototype.listen = function *listen() {
+    yield this.server.listen(this.options.port, this.options.host);
+};
+
+HttpTransport.prototype.close = function *close() {
+    yield this.server.close();
+};
+
+HttpTransport.prototype._handleRequest = function *_handleRequest(req, res) {
+    try {
+        // Authenticate
+        var token = (req.headers.authorization || '').split(/\s+/).pop() || '';
+        var auth = new Buffer(token, 'base64').toString();
+        var requestSecret = auth.split(/:/).shift() || '';
+
+        if (this.options.secret !== undefined && requestSecret !== this.options.secret) {
+            throw new WrongSecretError('Wrong secret!');
+        }
+
+        // Get params
+        var parsedUrl = url.parse(req.url, true);
+
+        var actionName = parsedUrl.pathname.substring(1);
+
+        if (!this.skinny.actions[actionName]) {
+            throw new ActionNotPresent('Action "' + actionName + '" not present!');
+        }
+
+        var body = yield this._receiveBody(req);
+        var bodyParams = body !== '' ? JSON.parse(body) : {};
+        var params = util._extend(bodyParams, parsedUrl.query);
+
+        // Run action
+        var actionSkinny = this.skinny.newSkinny();
+
+        actionSkinny.httpRequest = req;
+        actionSkinny.httpResponse = res;
+
+        var actionResponse = yield this.skinny.actions[actionName](params, actionSkinny);
+
+        if (!res.headersSent) {
+            if (!res.getHeader('Content-Type')) {
+                res.setHeader('Content-Type', 'application/json');
+            }
+
+            res.writeHead(res.statusCode || 200);
+        }
+
+        if (actionResponse !== undefined) {
+            res.write(JSON.stringify(actionResponse));
+        }
+
+        res.end();
+    } catch (e) {
+        this.skinny.emit('warning', e);
+
+        if (e instanceof this.skinny.actions.ERRORS.ActionsError) {
+            e = new BadRequestError(e.name + ': ' + e.message);
+        } else if (!(e instanceof HttpTransportError)) {
+            e = new ServiceError(e.name + ': ' + e.message);
+        }
+
+        var response = JSON.stringify({ error: e.name, errorDescription: e.message });
+
+        res.writeHead(e.responseCode, { 'Content-Type': 'application/json' });
+        res.end(response);
+    }
+};
+
+HttpTransport.prototype._createServer = function _createServer() {
+    "use strict";
+
+    var server = http.createServer(co(this._handleRequest).bind(this));
+
+    server.listen = thunkify(server.listen);
+    server.close = thunkify(server.close);
+
+    return server;
+};
+
+HttpTransport.prototype._receiveBody = thunkify(function _receiveBody(req, callback) {
+    "use strict";
+
+    var bodyString = '';
+
+    req.on('data', function(chunk) {
+        bodyString += chunk;
+    });
+
+    req.on('end', function() {
+        callback(null, bodyString);
+    });
+});
+
+module.exports = HttpTransport;
